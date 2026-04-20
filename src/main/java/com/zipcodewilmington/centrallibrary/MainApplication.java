eb0b2c274178c89b457009ba94575398c812d105
prachi0716 <prachiptl1707@gmail.com>
2026-04-20 15:07:03 -0400

    Interface created

--- src/main/java/com/zipcodewilmington/centrallibrary/MainApplication.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/MainApplication.java
@@ -1,8 +1,18 @@
-package com.zipcodewilmington.centralibrary;
+package com.zipcodewilmington.centrallibrary;
 
 /**
  * Created by n3pjk on 6/9/2025.
  */
 public class MainApplication {
+
+    public interface Searchable {
+        boolean matchesKeyword(String keyword);
+    }
+
+    public interface Reservable {
+        void reserve();
+
+        void cancelReserve();
+    }
 
 }
