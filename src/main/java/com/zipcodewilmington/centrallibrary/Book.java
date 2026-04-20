fa8a06e900ae4e1e4c63e53f88e155e7d2b028a6
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-04-20 17:24:41 -0400

    finished book.java and made slight changes

--- src/main/java/com/zipcodewilmington/centrallibrary/Book.java
+++ src/main/java/com/zipcodewilmington/centrallibrary/Book.java
@@ -1,4 +1,4 @@
-package com.zipcodewilmington.centrallibrary;
+package com.zipcodewilmington.OneLibrary;
 
 public class Book extends LibraryItem {
     private String author;
@@ -6,8 +6,86 @@ public class Book extends LibraryItem {
     private int pages;
     private String genre;
 
-    private void setId() {
+    public Book(String id, String title, String location,
+            String author, String isbn, int pages, String genre) {
 
+        setId(id);
+        setTitle(title);
+        setLocation(location);
+
+        setAuthor(author);
+        setIsbn(isbn);
+        setPages(pages);
+        setGenre(genre);
     }
-    
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        if(isbn != null && !isbn.isEmpty()) {
+            this.isbn = isbn;
+        } else {
+            System.out.println("Invalid ISBN");
+        }
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        if(pages > 0) {
+            this.pages = pages;
+        } else {
+            System.out.println("Invalid Pages");
+        }
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        if(genre != null && !genre.isEmpty()) {
+            this.genre = genre;
+        } else {
+            System.out.println("Invalid Genre");
+        }
+    }
+ 
+    @Override
+    public String[] getSearchableFields() {
+        return new String[] {
+        getTitle(),
+        author,
+        genre,
+        isbn
+        };
+    }
+
+    @Override
+    public double calculateLateFee(int daysLate) {
+        return daysLate * 0.5;
+    }
+
+    @Override
+    public int getMaxBorrowDays() {
+        return 14;
+    }
+
+    @Override
+    public String getItemType() {
+        return "book";
+    }
+
 }
