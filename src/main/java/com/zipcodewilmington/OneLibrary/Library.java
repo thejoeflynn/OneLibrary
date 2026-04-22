8bc039f59a99ed545ce8322490123a328fe9473f
michaelgiovanisie <siemichaelgiovani@gmail.com>
2026-04-22 12:56:35 -0400

    finished library

---
+++ src/main/java/com/zipcodewilmington/OneLibrary/Library.java
@@ -0,0 +1,68 @@
+package com.zipcodewilmington.OneLibrary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<LibraryItem> items;
+    private List<Member> members;
+    private List<Librarian> librarians;
+
+    public Library () {
+        items = new ArrayList<>();
+        members = new ArrayList<>();
+        librarians = new ArrayList<>();
+    }
+
+    public void addItem(LibraryItem item) {
+        items.add(item);
+    }
+
+    public void removeItem(LibraryItem item) {
+        items.remove(item);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public void addLibrarian(Librarian librarian) {
+        librarians.add(librarian);
+    }
+
+    public List<LibraryItem> search(String keyword) {
+        List<LibraryItem> results = new ArrayList<>();
+        for(LibraryItem item : items){
+            if(item.matchesKeyword(keyword)){
+            results.add(item);
+            }
+        }
+        return results;
+    }
+
+    public void display() {
+        for(LibraryItem item : items) {
+            String type = item.getItemType();
+            String title = item.getTitle();
+            String availability = item.isAvailable() ? "Available" : "Unavailable";
+            System.out.println(type + " " + title + " - " + availability);
+        }
+    }
+
+    public void lateReport() {
+        int daysLate = 3;
+        
+        for(Member member : members) {
+            System.out.println(member.getName());
+            for(LibraryItem item : member.getBorrowedItems()) {
+                String type = item.getItemType();
+                int axDays = item.getMaxBorrowDays();
+                double lateFees = item.calculateLateFee(daysLate);
+                System.out.println(type + " | Max Days: " + maxDays + " | Fee: " + lateFees);
+            }
+        }
+    }
+
+
+}
