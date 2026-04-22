77a155b0410e14de23ca15f8edfb66e2cc841c24
thejoeflynn <thejoeflynn@gmail.com>
2026-04-21 21:10:05 -0400

    UI mockup

--- src/main/java/com/zipcodewilmington/OneLibrary/MainApplication.java
+++ src/main/java/com/zipcodewilmington/OneLibrary/MainApplication.java
@@ -4,9 +4,50 @@ package com.zipcodewilmington.OneLibrary;
  * Created by n3pjk on 6/9/2025.
  */
 
+// psuedocode!
+// [ONELIBRARY] Welcome to the OneLibrary! Please login:
+// > (USER): m101
 
+// [ONELIBRARY] Welcome (Name) What would you like to do?
+//   |BOOKS|  |DVDs|  |CHECKOUT HISTORY|  |PROFILE|  |HELP|
 
+// > BOOKS
 
+// [ONELIBRARY] Books! 
+//   |SEARCH|  |FULL LIST|  |YEAR|  |AUTHOR|
 
+// > SEARCH
 
-}
+// [ONELIBRARY] Please search
+
+// > HARRY POTTER
+
+// [ONELIBRARY] Here's a list of books with "HARRY POTTER"
+//      BK0001 | Harry Potter and the Sorcerer's Stone, 1997-11-01, JK ROWLING
+//      BK0002 | Harry Potter and the Chamber of Secrets, 1998-10-31, JK ROWLING
+//      BK0003 | Harry Potter and the Prisoner of Azkaban, 1999-12-25, JK ROWLING
+
+// > BK0003
+
+// [ONELIBRARY] Harry Potter and the Prisoner of Azkaban
+//      DATE | 1999-12-25
+//    AUTHOR | JK ROWLING
+//        ID | BK0003
+//   SUMMARY | "Harry Potter goes on another wacky adventure to do whatever idk wizard stuff."
+// AVAILABLE | RESERVED UNTIL [2026-04-28]
+//
+//
+//
+//   |FAVORITE|  |RESERVE|  |BACK|  |HOME|
+//
+//
+// > BACK
+//
+
+// [ONELIBRARY] Welcome (Name) What would you like to do?
+//   |BOOKS|  |DVDs|  |CHECKOUT HISTORY|  |PROFILE|  |HELP|
+//
+//
+//
+// > CHECKOUT HISTORY
+//
