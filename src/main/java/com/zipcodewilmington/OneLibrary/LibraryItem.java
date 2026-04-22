2dbbb85aa5e081e34536b594f1519a0398c6117a
collin-beck3 <collin.beck3@gmail.com>
2026-04-21 20:16:33 -0400

    made periodical class w/ comments

--- src/main/java/com/zipcodewilmington/OneLibrary/LibraryItem.java
+++ src/main/java/com/zipcodewilmington/OneLibrary/LibraryItem.java
@@ -1,12 +1,20 @@
 package com.zipcodewilmington.OneLibrary;
 
-public class LibraryItem {
-    
+
 public abstract class LibraryItem {
     private String id;
     private String title;
     private String location;
     private boolean isAvailable;
+
+    // I need this for my periodical class but cant figure out how to get it to work correctly 
+
+    //public LibraryItem(String id, String title, String location) {
+        //setID(id); 
+        //setTitle(title); 
+        //setLocation(location); 
+        //this.isAvailable = true; 
+    //}
 
     public String getId() {
         return id;
@@ -57,10 +65,11 @@ public abstract class LibraryItem {
     }
 
     public boolean matchesKeyword(String keyword) {
-        if (keyword == null || keyword.isEmpty())
+        if (keyword == null || keyword.isEmpty()) {
             return false;
-        String[] fields = getSearchableFields();
-        for (String field : fields) {
+        }
+        //String[] fields = getSearchableFields();
+        for (String field : getSearchableFields()) {
             if (field != null && field.toLowerCase().contains(keyword.toLowerCase())) {
                 return true;
             }
@@ -76,5 +85,6 @@ public abstract class LibraryItem {
 
     public abstract String getItemType();
 
-}
-}
+    
+    }
+
