0507e8bc81cb7575d85113ac1d91ac0718508605
thejoeflynn <thejoeflynn@gmail.com>
2026-04-21 21:37:15 -0400

    Updated address

--- src/main/java/com/zipcodewilmington/OneLibrary/MainApplication.java
+++ src/main/java/com/zipcodewilmington/OneLibrary/MainApplication.java
@@ -4,9 +4,59 @@ package com.zipcodewilmington.OneLibrary;
  * Created by n3pjk on 6/9/2025.
  */
 
+// psuedocode!
+// [ONELIBRARY] Welcome to the OneLibrary! Please login:
+// > (USER): m101
+
+// [ONELIBRARY] Welcome (Name) What would you like to do?
+//   |BOOKS|  |DVDs|  |CHECKOUT HISTORY|  |PROFILE|  |HELP|
+
+// > BOOKS
+
+// [ONELIBRARY] Books! 
+//   |SEARCH|  |FULL LIST|  |YEAR|  |AUTHOR|
+
+// > SEARCH
+
+// [ONELIBRARY] Please search
+
+// > HARRY POTTER
+
+// [ONELIBRARY] Here's a list of books with "HARRY POTTER"
+//      BK0001 | Harry Potter and the Sorcerer's Stone, 1997-11-01, JK ROWLING
+//      BK0002 | Harry Potter and the Chamber of Secrets, 1998-10-31, JK ROWLING
+//      BK0003 | Harry Potter and the Prisoner of Azkaban, 1999-12-25, JK ROWLING
+
+// > BK0003
+
+// [ONELIBRARY] Harry Potter and the Prisoner of Azkaban
+//      DATE | 1999-12-25
+//    AUTHOR | JK ROWLING
+//        ID | BK0003
+//   SUMMARY | "Harry Potter goes on another wacky adventure to do whatever idk wizard stuff."
+// AVAILABLE | RESERVED UNTIL [2026-04-28]
+//
+//
+//
+//   |FAVORITE|  |RESERVE|  |BACK|  |HOME|
+//
+//
+// > BACK
 
 
-
-
-
-}
+// [ONELIBRARY] Welcome (Name) What would you like to do?
+//   |BOOKS|  |DVDs|  |CHECKOUT HISTORY|  |PROFILE|  |HELP|
+//
+// > CHECKOUT HISTORY
+//
+// [ONELIBRARY] Things you have previously checked-out
+//      DV6180 | The Dark Side of the Moon | RSVD: 2026-04-20 | RTRN: 2026-04-21
+//      BK2349 | The Great Gatsby | RSVD: 2026-04-09 | RTRN: 2026-04-17
+//      BK0047 | The Essential Calvin and Hobbes | RSVD: 2026-04-01 | RTRN: 2026-04-19
+//      DV5710 | To Pimp a Butterfly | RSVD: 2026-04-02 | RTRN: 2026-04-04
+//
+//
+//
+//   |RE-RESERVE|  |BACK|  |HOME|
+//
+// > BACK
