10814c18ed945a57f2940131c2d10e6fdae36bbe
thejoeflynn <thejoeflynn@gmail.com>
2026-04-28 13:32:55 -0400

    finalize

--- src/main/java/com/zipcodewilmington/OneLibrary/DataLoader.java
+++ src/main/java/com/zipcodewilmington/OneLibrary/DataLoader.java
@@ -16,6 +16,7 @@ public class DataLoader {
     loadBooks(basePath + "Book.csv", library);
     loadDVDs(basePath + "DVD.csv", library);
     loadPeriodicals(basePath + "Periodical.csv", library);
+    loadMusic(basePath + "Music.csv", library);
 
         // Periodical Temporarily disabled to get the thing working
         //loadPeriodicalTitles("periodical-titles.csv");
@@ -171,6 +172,54 @@ public class DataLoader {
         e.printStackTrace();
     }
 }
+
+private void loadMusic(String filePath, Library library) {
+
+        try (CSVReader reader = new CSVReader(new FileReader(filePath))) {
+
+            String[] parts;
+            reader.readNext(); 
+
+            while ((parts = reader.readNext()) != null) {
+
+                if (parts.length < 8) {
+                    System.out.println("Skipping invalid Music row: ");
+                    continue;
+                }
+
+                String id = parts[0].trim();
+                String title = parts[1].trim();
+                String location = parts[2].trim();
+                String artist = parts[3].trim();
+                String date = parts[4].trim();
+                String genre = parts[5].trim();
+                String lyrics = parts[6].trim();
+                String lengthA = parts[7].trim();
+
+                try {
+                    int length = Integer.parseInt(lengthA);
+                    Music music = new Music(
+                            id,
+                            title,
+                            location,
+                            artist,
+                            date,
+                            genre,
+                            lyrics,
+                            length
+                    );
+
+                    library.addItem(music);
+
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid duration in row: ");
+                }
+            }
+
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+    }  
 
     public static void main(String[] args) {
         DataLoader loader = new DataLoader();
